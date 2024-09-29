5ef44437669586a162783f20a8d8b8fe976f12a9
Bhargava, Tarang <bhargava.tarang20@gmail.com>
2024-09-29 16:37:23 +0100

    Initial HelloWorld application commit.

---
+++ src/main/java/com/titania/activitydashboard/Application.java
@@ -0,0 +1,12 @@
+package com.titania.activitydashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/com/titania/activitydashboard/ApplicationTest.java
@@ -0,0 +1,12 @@
+package com.titania.activitydashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivityDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
